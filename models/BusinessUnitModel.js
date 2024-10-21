0b60cdc81b9f8556670a1a29ffb1c318630de625
faizbyp <faizbyp@gmail.com>
2024-10-21 17:10:02 +0700

    config

---
+++ models/BusinessUnitModel.js
@@ -0,0 +1,13 @@
+const db = require("../config/connection");
+const TRANS = require("../config/transaction");
+const { insertQuery, updateQuery } = require("../helper/queryBuilder");
+
+const createBusinessUnit = async () => {};
+
+const readBusinessUnit = async () => {};
+
+const updateBusinessUnit = async () => {};
+
+const deleteBusinessUnit = async () => {};
+
+module.exports = { createBusinessUnit, readBusinessUnit, updateBusinessUnit, deleteBusinessUnit };
