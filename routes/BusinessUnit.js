1911c317701a81f7f78c470f1a641edbde64da5b
faizbyp <faizbyp@gmail.com>
2024-10-22 09:43:14 +0700

    feat: update bu

--- routes/BusinessUnit.js
+++ routes/BusinessUnit.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   handleReadBusinessUnit,
   handleCreateBusinessUnit,
+  handleUpdateBusinessUnit,
 } = require("../controllers/BusinessUnitController");
 const router = express.Router();
 
 router.get("/", handleReadBusinessUnit);
 router.post("/", handleCreateBusinessUnit);
+router.patch("/:id_business_unit", handleUpdateBusinessUnit);
 
 module.exports = router;
