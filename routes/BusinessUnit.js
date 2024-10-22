e9f0065ff4095ad8b30f15e0ee78ba8b7a4ca0b0
faizbyp <faizbyp@gmail.com>
2024-10-22 09:27:32 +0700

    feat: create business unit

--- routes/BusinessUnit.js
+++ routes/BusinessUnit.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { handleReadBusinessUnit } = require("../controllers/BusinessUnitController");
+const {
+  handleReadBusinessUnit,
+  handleCreateBusinessUnit,
+} = require("../controllers/BusinessUnitController");
 const router = express.Router();
 
 router.get("/", handleReadBusinessUnit);
+router.post("/", handleCreateBusinessUnit);
 
 module.exports = router;
