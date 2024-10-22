07df4cf0cfe1c9a4133a8a9de9697f34efa217a4
faizbyp <faizbyp@gmail.com>
2024-10-22 09:27:32 +0700

    feat: create business unit

--- controllers/BusinessUnitController.js
+++ controllers/BusinessUnitController.js
@@ -1,6 +1,26 @@
-const { readBusinessUnit } = require("../models/BusinessUnitModel");
+const { readBusinessUnit, createBusinessUnit } = require("../models/BusinessUnitModel");
+const { v4: uuidv4 } = require("uuid");
 
-const handleCreateBusinessUnit = async (req, res) => {};
+const handleCreateBusinessUnit = async (req, res) => {
+  const payload = {
+    id_business_unit: uuidv4(),
+    code_business_unit: req.body.code_business_unit,
+    name_business_unit: req.body.name_business_unit,
+    is_active: req.body.is_active,
+  };
+
+  try {
+    let result = await createBusinessUnit(payload);
+    res.status(200).send({
+      message: `Success add business unit`,
+      code: result,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+};
 
 const handleReadBusinessUnit = async (req, res) => {
   try {
--- models/BusinessUnitModel.js
+++ models/BusinessUnitModel.js
@@ -2,7 +2,22 @@ const db = require("../config/connection");
 const TRANS = require("../config/transaction");
 const { insertQuery, updateQuery } = require("../helper/queryBuilder");
 
-const createBusinessUnit = async () => {};
+const createBusinessUnit = async (payload) => {
+  const client = await db.connect();
+  try {
+    await client.query(TRANS.BEGIN);
+    const [q, v] = insertQuery("mst_business_unit", payload, "code_business_unit");
+    const result = await client.query(q, v);
+    await client.query(TRANS.COMMIT);
+    return result.rows;
+  } catch (error) {
+    console.log(error);
+    await client.query(TRANS.ROLLBACK);
+    throw error;
+  } finally {
+    client.release();
+  }
+};
 
 const readBusinessUnit = async () => {
   const client = await db.connect();
